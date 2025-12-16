68be52be1a8d391507f7245838070afbb370b8a9
Ayesha733 <h.ayeshasaddiqua@gmail.com>
2025-12-16 08:23:12 -0700

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,12 @@
+a=25
+print( "a=",a,type(a))
+
+b=25.7:fyqou
+print( "b=",b,type(b))
+
+c=True
+print( "c=",c,type(c))
+
+
+d="Hello"
+print( "d=",d,type(d))
---
+++ a2.py
@@ -0,0 +1,8 @@
+age=input ( "Enter  your age:")
+print("your age is:" , a)
+print("Datatype of a:" type (a)) 
+
+b=int(a)
+print("datatype of a after conversion :",  type (b)) 
+
+height=float(input
